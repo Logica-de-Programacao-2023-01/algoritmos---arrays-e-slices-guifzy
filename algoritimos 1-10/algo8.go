0c75f67d0a49fdb2ceea83d2c868946380f9e8f4
Guilherme Fernandes <guifrxx@gmail.com>
2023-04-16 14:56:37 -0300

    solução

--- algoritimos 1-10/algo8.go
+++ algoritimos 1-10/algo8.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() { //Crie um Slice de strings com tamanho 8 e solicite ao usuário que informe um valor. Remova todas as ocorrências desse valor no Slice e imprima o resultado.
 	slice := []string{"gui", "lucas", "felipe", "maria", "leo", "luisa", "bruno", "jao"}
-	var num string
+	var nome string
 
-	fmt.Printf("Informe o número a ser removido da lista %v:\n", slice)
-	fmt.Scan(&num)
+	fmt.Printf("Informe o nome a ser removido da lista: %v\n", slice)
+	fmt.Scan(&nome)
 
 	novoSlice := make([]string, 0, len(slice))
 	for _, elemento := range slice {
-		if elemento != num {
+		if elemento != nome {
 			novoSlice = append(novoSlice, elemento)
 		}
 	}
